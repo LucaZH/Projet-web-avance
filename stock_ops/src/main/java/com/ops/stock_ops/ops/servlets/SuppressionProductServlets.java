0c3c2445dedc5910a205f88085bd56724dfb5b6b
j03-dev <24nomeniavo@gmail.com>
2023-06-23 15:06:53 +0300

    Fix bug

--- stock_ops/src/main/java/com/ops/stock_ops/ops/servlets/SuppressionProductServlets.java
+++ stock_ops/src/main/java/com/ops/stock_ops/ops/servlets/SuppressionProductServlets.java
@@ -1,22 +1,28 @@
 package com.ops.stock_ops.ops.servlets;
 
+import com.ops.stock_ops.DatabaseConnection;
 import com.ops.stock_ops.client.daos.ProductDAO;
-import com.ops.stock_ops.client.ClientDatabaseConnection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 
 public class SuppressionProductServlets extends HttpServlet {
 
-    private final Connection connection = ClientDatabaseConnection.getInstance("");
-    public SuppressionProductServlets () {super();}
+    public SuppressionProductServlets() {
+        super();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String path = (String) session.getAttribute("path_db");
+        Connection connection = DatabaseConnection.getInstance(path);
+
         int id_product = Integer.valueOf(req.getParameter("id_product"));
         ProductDAO productDao = new ProductDAO(connection);
         if (productDao.delete(id_product)) {
